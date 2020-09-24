faad809d14dca61f020f903bc735bbd9506b73e1
Yagdra-nsk <Akela.Klimov@yandex.ru>
2020-09-25 00:53:22 +0700

    Some fixes for lab1 Done

--- Lab_1/src/CopySearcher.java
+++ Lab_1/src/CopySearcher.java
@@ -1,13 +1,13 @@
-import java.io.*;
+import java.io.IOException;
 import java.net.*;
-import java.text.*;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.logging.*;
 
 public class CopySearcher {
     private final static Logger logger = Logger.getLogger(CopySearcher.class.getName());
-    private final int port = 2048;
-    private final int receive_timeout = 1000;
+    private final int PORT = 2048;
+    private final int RECEIVE_TIMEOUT = 1000;
     private final Map<String, Long> currentIpTable = new HashMap<>();
     private InetAddress group;
     private MulticastSocket multicastSocket;
@@ -16,14 +16,12 @@ public class CopySearcher {
     public CopySearcher(String multicastIpAddr){
         try{
             group = InetAddress.getByName(multicastIpAddr);
-            multicastSocket = new MulticastSocket(port);
+            multicastSocket = new MulticastSocket(PORT);
             datagramSocket = new DatagramSocket();
             multicastSocket.joinGroup(group);
-            multicastSocket.setSoTimeout(receive_timeout);
+            multicastSocket.setSoTimeout(RECEIVE_TIMEOUT);
         }
         catch (IOException ex){
-            multicastSocket.close();
-            datagramSocket.close();
             ex.printStackTrace();
         }
     }
@@ -32,8 +30,8 @@ public class CopySearcher {
         logger.log(Level.INFO, "New host join group");
         while (true){
             try{
-                datagramSocket.send(new DatagramPacket("Packet Hello".getBytes(), "Packet Hello".getBytes().length, group, port));
-                long end = System.currentTimeMillis() + receive_timeout;
+                datagramSocket.send(new DatagramPacket("Packet Hello".getBytes(), "Packet Hello".getBytes().length, group, PORT));
+                long end = System.currentTimeMillis() + RECEIVE_TIMEOUT;
                 while (System.currentTimeMillis() < end){
                     byte[] buffer = new byte[256];
                     DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
@@ -57,27 +55,25 @@ public class CopySearcher {
                 break;
             }
         }
+
+        multicastSocket.close();
+        datagramSocket.close();
     }
 
     private void checkCurrentIpTable(){
-        for(Iterator<Map.Entry<String, Long>> iterator = currentIpTable.entrySet().iterator(); iterator.hasNext();){
-            Map.Entry<String, Long> entry = iterator.next();
-            int table_timeout = 5000;
-            if(System.currentTimeMillis() - entry.getValue() > table_timeout){
-                logger.log(Level.INFO, "Lost connection with: " + entry.getKey());
-                iterator.remove();
+        int table_timeout = 5000;
+        for(Map.Entry<String, Long> ip_plus_port : currentIpTable.entrySet()){
+            if(System.currentTimeMillis() - ip_plus_port.getValue() > table_timeout){
+                logger.log(Level.INFO, "Lost connection with: " + ip_plus_port.getKey());
+                currentIpTable.remove(ip_plus_port.getKey());
                 printCurrentIPTable();
             }
         }
     }
 
     private void checkIpState(String ip_plus_port){
-        if(!currentIpTable.containsKey(ip_plus_port)){
-            currentIpTable.put(ip_plus_port, System.currentTimeMillis());
+        if(currentIpTable.put(ip_plus_port, System.currentTimeMillis()) == null){
             printCurrentIPTable();
-        }
-        else {
-            currentIpTable.put(ip_plus_port, System.currentTimeMillis());
         }
     }
 
@@ -85,9 +81,7 @@ public class CopySearcher {
         SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss");
         Date date = new Date(System.currentTimeMillis());
         System.out.println("Table update:" + formatter.format(date));
-        for(Map.Entry<String, Long> entry : currentIpTable.entrySet()){
-            System.out.println("IP:" + entry.getKey());
-        }
+        currentIpTable.forEach((key, value) -> System.out.println("IP:" + key));
         System.out.println();
     }
 }
